26eea0a0871443c83fbd0f587aae9e458ee7dc36
Lisa Dafny <lisadafny.tech@gmail.com>
2022-12-31 19:37:25 -0300

    setting the route for mvc

--- src/Site.UI/Program.cs
+++ src/Site.UI/Program.cs
@@ -1,7 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
