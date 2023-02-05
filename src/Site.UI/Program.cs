4970af5d717983d0f7f382d14ea0cae5bac7d3a0
Lisa Dafny <lisadafny.tech@gmail.com>
2023-02-05 18:40:58 -0300

    configure db using DbContext

--- src/Site.UI/Program.cs
+++ src/Site.UI/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using Site.UI.Config.Command;
 using Site.UI.Config.Repository;
 using Site.UI.Config.Service;
+using Site.UI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddDbContext<DataDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DataDbContext")));
 
 builder.Services.ConfigCommand();
 builder.Services.ConfigService();
